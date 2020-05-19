164d58193cc922a67e144c8a2035773da8aed7e8
jordan.mbompieze <jordan.mbompieze@sprint-pay.com>
2020-05-19 19:49:29 +0100

    Initial version of myapp generated by JHipster-6.9.0

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import './vendor';
+import { MyappSharedModule } from 'app/shared/shared.module';
+import { MyappCoreModule } from 'app/core/core.module';
+import { MyappAppRoutingModule } from './app-routing.module';
+import { MyappHomeModule } from './home/home.module';
+import { MyappEntityModule } from './entities/entity.module';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import { MainComponent } from './layouts/main/main.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
+import { ErrorComponent } from './layouts/error/error.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    MyappSharedModule,
+    MyappCoreModule,
+    MyappHomeModule,
+    // jhipster-needle-angular-add-module JHipster will add new module here
+    MyappEntityModule,
+    MyappAppRoutingModule,
+  ],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent],
+  bootstrap: [MainComponent],
+})
+export class MyappAppModule {}
